be4cab6184059ac78ce364d272ce157232354b0b
yh932514 <yh932514@ohio.edu>
2019-01-29 01:30:40 -0500

    Create registerHandlers.js

---
+++ registerHandlers.js
@@ -0,0 +1,27 @@
+/*Fix the bugs in the registerHandlers function. An alert should display anchor's zero-based index within a document instead of following the link.
+
+For example, in the document below, the alert should display "2" when Google anchor is clicked since it is the third anchor element in the document and its zero-based index is 2.*/
+
+registerHandlers();
+
+function registerHandlers() {
+  var as = document.getElementsByTagName('a');
+  var links = new Array;
+  
+  for (var i = 0; i < as.length; i++) {
+ 	 links[i]=as[i].innerText;
+    as[i].onclick = function() {
+      alert(links.indexOf(this.innerText));
+      return false;
+    }
+  }
+}
+
+/* HTML code for testing purposes (do not submit uncommented):
+<body>
+  In my life, I used the following web search engines:<br/>
+  <a href="//www.yahoo.com">Yahoo!</a><br/>
+  <a href="//www.altavista.com">AltaVista</a><br/>
+  <a href="//www.google.com">Google</a><br/>
+</body>
+*/
